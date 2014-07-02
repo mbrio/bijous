47819057bea41def2ada9cccba4f8deb97673ff4
Michael Diolosa <michael.diolosa@gmail.com>
2014-07-01 22:35:03 -0400

    Add code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,24 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
-gulp.task('lint', function gulpLint() {
-  return gulp.src(['./*.js', './src/**/*.js', './test/**/*.js'])
+gulp.task('lint', function gulpLint(cb) {
+  gulp.src(['./*.js', './test/**/*.js'])
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter('default'))
+    .on('end', cb);
 });
 
-gulp.task('test', ['lint'], function gulpTest() {
-  return gulp.src(['./test/**/*-test.js'])
-    .pipe(mocha({ reporter: 'spec' }));
+gulp.task('test', ['lint'], function gulpCoverage(cb) {
+  gulp.src(['./index.js'])
+    .pipe(istanbul())
+    .on('finish', function () {
+      gulp.src(['./test/**/*-test.js'])
+        .pipe(mocha())
+        .pipe(istanbul.writeReports())
+        .on('end', cb);
+    });
 });
 
 gulp.task('default', ['test']);
